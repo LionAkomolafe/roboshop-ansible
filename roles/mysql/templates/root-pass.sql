b9f46f16fc0f55d3e4d7e170d61ce0f7a91872f7
Yemi <tywonderkid@yahoo.com>
2023-03-06 18:33:12 -0600

    Ansible script

--- roles/mysql/templates/root-pass.sql
+++ roles/mysql/templates/root-pass.sql
@@ -1,3 +1,1 @@
 ALTER USER 'root'@'localhost' IDENTIFIED BY '{{ROOT_PASSWORD}}';
-ALTER USER 'root'@'%' IDENTIFIED BY '{{ROOT_PASSWORD}}';
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
