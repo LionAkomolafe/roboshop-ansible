1164e1b503758017ef57b4da715c681bba6b2867
Yemi <tywonderkid@yahoo.com>
2023-03-06 18:51:19 -0600

    Ansible script

--- roles/mysql/templates/root-pass.sql
+++ roles/mysql/templates/root-pass.sql
@@ -1,1 +1,4 @@
 ALTER USER 'root'@'localhost' IDENTIFIED BY '{{ROOT_PASSWORD}}';
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'RoboShop@1';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
